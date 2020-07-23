46400143a5a70240024b2ce6bbcf4fb887ca0d57
Travis Fox <travis.fox90@gmail.com>
2020-07-23 03:18:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song
 
-  attr_reader :name
+  attr_reader :name, :artist, :genre
   
   @@all = []
   
   def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
     save
   end
 
