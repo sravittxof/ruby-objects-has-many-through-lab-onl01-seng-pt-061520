6ce5c5fbc2fd094a39641c9f2fd19c84bb31aaad
Travis Fox <travis.fox90@gmail.com>
2020-07-23 03:17:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,21 @@
 class Song
+
+  attr_reader :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end  
   
   
   
