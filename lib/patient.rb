6381b4907d4855de98d5ab7cab160bc6bbae78e5
Travis Fox <travis.fox90@gmail.com>
2020-07-23 03:33:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,6 +13,16 @@ class Patient
     @@all << self
   end      
   
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments
+    Appointment.all.select { |appointments| appointments.doctor.name == self.name }
+  end  
+  
+  
+  
   
   
 end
